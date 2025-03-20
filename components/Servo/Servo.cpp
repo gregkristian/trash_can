f62781a2d0bef68d9709de0ad614484bb2186368
gregkristian <gkristianp@gmail.com>
2025-03-20 17:12:14 +0100

    Fix Servo

--- components/Servo/Servo.cpp
+++ components/Servo/Servo.cpp
@@ -7,10 +7,11 @@
 
 static const char *TAG = "Servo";
 
-#define SERVO_MIN_PULSEWIDTH 500   // Minimum pulse width in microseconds
+#define SERVO_MIN_PULSEWIDTH 1000  // Minimum pulse width in microseconds
 #define SERVO_MAX_PULSEWIDTH 2500  // Maximum pulse width in microseconds
 #define SERVO_MAX_DEGREE 180       // Maximum angle in degrees
 #define SERVO_GPIO_PIN 15          // GPIO pin connected to the servo
+#define SERVO_FREQ 50              // Frequency of the PWM signal
 
 void Servo::servo_init()
 {
@@ -18,7 +19,7 @@ void Servo::servo_init()
         .speed_mode = LEDC_LOW_SPEED_MODE,
         .duty_resolution = LEDC_TIMER_16_BIT,
         .timer_num = LEDC_TIMER_0,
-        .freq_hz = 50, // 50Hz PWM frequency
+        .freq_hz = SERVO_FREQ,
         .clk_cfg = LEDC_AUTO_CLK
     };
     ledc_timer_config(&timer_conf);
@@ -37,13 +38,22 @@ void Servo::servo_init()
 
 void Servo::servo_set_angle(int angle)
 {
-    ESP_LOGI(TAG, "set angle %d cm", angle);
-    if (angle < 0) angle = 0;
-    if (angle > SERVO_MAX_DEGREE) angle = SERVO_MAX_DEGREE;
+    // First, limit the angle to the range 0 - 180 degrees
+    if (angle < 0) {
+        angle = 0;
+    } else if (angle > SERVO_MAX_DEGREE) {
+        angle = SERVO_MAX_DEGREE;
+    } 
     
-    uint32_t duty = SERVO_MIN_PULSEWIDTH + ((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * angle) / SERVO_MAX_DEGREE;
-    duty = (duty * (1 << 16)) / 20000; // Convert to LEDC duty cycle (16-bit resolution, 20ms period)
-    ESP_LOGI(TAG, "set angle duty=%lu ", duty);
+    // Calculate pulse_width. Range is 1 to 2ms, 0 to 180 degrees
+    // See http://www.ee.ic.ac.uk/pcheung/teaching/DE1_EE/stores/sg90_datasheet.pdf
+    int pulse_width = SERVO_MIN_PULSEWIDTH + ((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * angle) / SERVO_MAX_DEGREE;
+    
+    // Convert pulse_width to LEDC duty cycle (16-bit resolution, 20ms period)
+    int duty = (pulse_width * (1 << 16)) / 20000;
+    ESP_LOGI(TAG, "angle=%d pulse_width=%d duty=%d ", angle, pulse_width, duty);
+
+    // Execute the duty cycle on pin connected to servo
     ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty);
     ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
 }
