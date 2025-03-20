a485c5ede097cdcb94d0656906fc2437dd0aade6
gregkristian <gkristianp@gmail.com>
2025-03-20 20:42:10 +0100

    Adding led lamp

--- main/main.cpp
+++ main/main.cpp
@@ -12,6 +12,7 @@
 #define DELAY_MS        500  // Delay in main loop
 #define DISTANCE_SENSOR 10   // Distance in cm to open the lock
 #define TIMEOUT_LOCK    2000 // Open for 2 seconds then close if sensor doesn't detect anything
+#define LED_PIN         (gpio_num_t)4    // GPIO pin for LED
 
 static const char *TAG = "Main";
 
@@ -22,11 +23,16 @@ extern "C" void app_main(void)
     UltrasonicSensor UltrasonicSensor((gpio_num_t)23, (gpio_num_t)20);
     Servo servo;
     servo.servo_init();
-    servo.servo_set_angle(100);
+    servo.servo_set_angle(160);
     vTaskDelay(pdMS_TO_TICKS(1000));
 
     int timer = TIMEOUT_LOCK;
     bool lock_is_open = false;
+
+    // Init LED
+    gpio_reset_pin(LED_PIN); 
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT); 
+    gpio_set_level(LED_PIN, false);  // Turn off LED
     
     while(1) {
         long dist = UltrasonicSensor.measureDistance();
@@ -36,6 +42,7 @@ extern "C" void app_main(void)
         if (dist < DISTANCE_SENSOR && lock_is_open == false) {
             ESP_LOGI(TAG, "Object detected, distance %ld cm. OPENING LOCK", dist);
             servo.servo_set_angle(0);
+            gpio_set_level(LED_PIN, true);  // Turn on LED
             lock_is_open = true;
         }
         
@@ -48,6 +55,7 @@ extern "C" void app_main(void)
             if (timer <= 0) {
                 ESP_LOGI(TAG, "Timeout. CLOSING LOCK");
                 servo.servo_set_angle(160);
+                gpio_set_level(LED_PIN, false);  // Turn off LED
                 lock_is_open = false;
                 timer = TIMEOUT_LOCK;
             }
